82cff8177a8653ab8ec82e70f516506e6915ed16
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-01-16 16:09:52 -0300

    cors and helmet

--- src/app.js
+++ src/app.js
@@ -2,15 +2,27 @@ import 'dotenv/config';
 
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 import path from 'path';
 import routes from './routes';
 
 import './database';
 
+const whitelist = [
+    'http://localhost:3000',
+    'https://app.netlify.com/sites/nervous-kowalevski-6308c2/overview',
+];
+
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200,
+    origin(origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
 };
+
 class App {
     constructor() {
         this.server = express();
@@ -20,6 +32,13 @@ class App {
     }
 
     middlewares() {
+        this.server.use(
+            helmet.contentSecurityPolicy({
+                directives: {
+                    ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+                },
+            })
+        );
         this.server.use(cors(corsOptions));
         this.server.use(express.json());
         this.server.use(
